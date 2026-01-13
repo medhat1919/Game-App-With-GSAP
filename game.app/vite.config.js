348e6cb09f4d414a78d4f957cb59b01e99cf1828
medhat <medhatmoris90@gmail.com>
2026-01-13 23:03:11 +0200

    Update homepage and vite base for new repo deployment

--- game.app/vite.config.js
+++ game.app/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Animation.app/',
-  plugins: [react() , tailwindcss()],
-  
+  base: '/Game-App-With-GSAP/',
+  plugins: [react(), tailwindcss()],
+
 })
